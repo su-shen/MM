daf8dee4614f698c465c9927627cf8ce30653ce4
xiaomage <xiaomage@qq.com>
2016-05-19 21:49:23 +0800

    初始化操作

---
+++ MM/MM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MM
+//
+//  Created by sy on 16/5/19.
+//  Copyright © 2016年 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MM/MM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MM
+//
+//  Created by sy on 16/5/19.
+//  Copyright © 2016年 sy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"hello word");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
